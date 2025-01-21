e129de7a7d2a10a1a09ee752c34bc84245ceafea
grexrr <wyatt_h.w@protonmail.com>
2025-01-21 09:36:37 +0000

    test mapper

--- mapper.py
+++ mapper.py
@@ -1,5 +1,11 @@
 import sys
-for line in sys.stdin:
-    words = line.strip().split()
-    for word in words:
-        print(f"{word}\t1")
+
+def mapper():
+    for line in sys.stdin:
+        line = line.strip()
+        words = line.split()    
+        for word in words:
+            print(f'{word}\t1')
+
+if __name__ == "__main__":
+    mapper()
--- test1.py
+++
@@ -1,5 +0,0 @@
-def test_environment():
-    print("Python virtual environment is working!")
-
-if __name__ == "__main__":
-    test_environment()
